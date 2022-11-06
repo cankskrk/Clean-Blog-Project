3208e48dbc0d28cbbf597ea5792fedfa6aa07482
cankskrk <ardacankisakurek@gmail.com>
2022-11-06 03:29:29 +0100

    Update Workspace

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog Title',
-    description: 'Blog Description',
-  };
+// View Engine
+app.set('view engine', 'ejs');
 
-  res.send(blog);
+// Middlewares
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(port, () => {
